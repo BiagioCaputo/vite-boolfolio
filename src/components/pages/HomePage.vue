41eb15bfba69ff44f3c9fae3894d94192a8ee5c2
BiagioCaputo <scebiagio98@gmail.com>
2024-04-02 23:34:13 +0200

    added pagination and created a basepagination component

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <script>
 /****************  Componenti template *****************/
 import ProjectCard from '../projects/ProjectCard.vue';
+import BasePagination from '../BasePagination.vue';
+
 
 import axios from 'axios';
 const baseEndpoint = 'http://localhost:8000/api/projects/'
@@ -9,15 +11,24 @@ export default {
     name: 'HomePage',
     data() {
         return {
-            projects: []
+            projects: {
+                data: [],
+                links: []
+            },
         }
     },
-    components: { ProjectCard },
+    components: { ProjectCard, BasePagination },
     methods: {
-        fetchProjects() {
-            axios.get(baseEndpoint).then((res) => {
-                this.projects = res.data
-            })
+        fetchProjects(endpoint) {
+            if (!endpoint) endpoint = baseEndpoint;
+            axios.get(endpoint)
+                .then((res) => {
+                    const { data, links } = res.data;
+                    this.projects = { data, links }; //posso fare cosi invece che data:data e links:links perchè chiave e variabile sono uguali
+                }).catch((err) => {
+                    console.log(err)
+                }).then(() => {
+                })
         }
     },
     created() {
@@ -30,8 +41,10 @@ export default {
 
     <h1 class="text-center my-5"> I miei progetti</h1>
     <div class="row row-cols-3">
-        <ProjectCard v-for="project in projects" :project="project" :isProjectShow="false" />
+        <ProjectCard v-for="project in projects.data" :project="project" :isProjectShow="false" />
+        <BasePagination :links="projects.links" @change-page="fetchProjects" />
     </div>
+
 
 </template>
 
